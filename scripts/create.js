97b88aab52553d259c81398c9031a1bb8ca11bed
wujimin515600 <1260299814@qq.com>
2025-01-27 15:55:52 +0800

    Update create.js

--- scripts/create.js
+++ scripts/create.js
@@ -3,8 +3,10 @@ import MarkdownIt from 'markdown-it'
 import { getTokens, getDependencies } from './utils.js'
 import path from 'path';
 
+const workspace = process.env.GITHUB_WORKSPACE;
 if (typeof process !== 'undefined' && process.versions != null && process.versions.node != null) {
   console.log('Running in Node.js environment');
+  console.log('workspace','workspace')
   // console.log(process.cwd())
   // const filepath = path.dirname('README.MD')
   // console.log('filepath', filepath)
